7bf160ff7cc829f2f45c8e74facac471e6c6eb80
sanky-cse <sankar@prmfincon.com>
2018-08-13 04:11:59 +0530

    error handled

--- src/backend-service/processing_code_b.py
+++ src/backend-service/processing_code_b.py
@@ -64,81 +64,87 @@ def download():
 
 @app.route('/dashboard/', methods=['GET'])
 def dashboard():
-    # reading the files
-    proc_list = []
-    with open(config.PROC_LOG) as proc_log:
-        for line in proc_log:
-            print(line[0:5])
-            if line[0:5]=='DEBUG':
-                temp_line = line.replace('DEBUG:flask.app:', '')
-                proc_list.append([col_val.strip() for col_val in temp_line.split('--')])
+    try:
+        # reading the files
+        proc_list = []
+        with open(config.PROC_LOG) as proc_log:
+            for line in proc_log:
+                print(line[0:5])
+                if line[0:5]=='DEBUG':
+                    temp_line = line.replace('DEBUG:flask.app:', '')
+                    proc_list.append([col_val.strip() for col_val in temp_line.split('--')])
 
-    cron_list = []
-    with open(config.CRON_LOG) as cron_log:
-        for line in cron_log:
-            cron_list.append([col_val.strip() for col_val in line.split('--')])
+        cron_list = []
+        with open(config.CRON_LOG) as cron_log:
+            for line in cron_log:
+                cron_list.append([col_val.strip() for col_val in line.split('--')])
 
-    # convert to dataframe
-    proc_df = pd.DataFrame(proc_list, columns=config.PROC_COL)
-    cron_df = pd.DataFrame(cron_list, columns=config.CRON_COL)
+        # convert to dataframe
+        proc_df = pd.DataFrame(proc_list, columns=config.PROC_COL)
+        cron_df = pd.DataFrame(cron_list, columns=config.CRON_COL)
 
-    # change dtypes of df
-    proc_df[['exist_col', 'new_col', 'doc_cnt']] = proc_df[['exist_col', 'new_col', 'doc_cnt']].apply(
-        pd.to_numeric
-    )
-    proc_df['proc_time'] = pd.to_datetime(proc_df['proc_time'], format='%Y/%m/%d %H:%M:%S')
-    proc_df['start_time'] = pd.to_datetime(proc_df['start_time'], format='%Y/%m/%d %H:%M:%S')
+        # change dtypes of df
+        proc_df[['exist_col', 'new_col', 'doc_cnt']] = proc_df[['exist_col', 'new_col', 'doc_cnt']].apply(
+            pd.to_numeric
+        )
+        proc_df['proc_time'] = pd.to_datetime(proc_df['proc_time'], format='%Y/%m/%d %H:%M:%S')
+        proc_df['start_time'] = pd.to_datetime(proc_df['start_time'], format='%Y/%m/%d %H:%M:%S')
 
-    cron_df[['row', 'col']] = cron_df[['row', 'col']].apply(pd.to_numeric)
-    cron_df['start_time'] = pd.to_datetime(cron_df['start_time'], format='%Y/%m/%d %H:%M:%S')
-    cron_df['trig_time'] = pd.to_datetime(cron_df['trig_time'], format='%Y/%m/%d %H:%M:%S')
+        cron_df[['row', 'col']] = cron_df[['row', 'col']].apply(pd.to_numeric)
+        cron_df['start_time'] = pd.to_datetime(cron_df['start_time'], format='%Y/%m/%d %H:%M:%S')
+        cron_df['trig_time'] = pd.to_datetime(cron_df['trig_time'], format='%Y/%m/%d %H:%M:%S')
 
-    # new diff of time column
-    proc_df['exec_time'] = (proc_df['proc_time']-proc_df['start_time'])/np.timedelta64(1, 's')
-    cron_df['exec_time'] = (cron_df['trig_time'] - cron_df['start_time'])/np.timedelta64(1, 's')
+        # new diff of time column
+        proc_df['exec_time'] = (proc_df['proc_time']-proc_df['start_time'])/np.timedelta64(1, 's')
+        cron_df['exec_time'] = (cron_df['trig_time'] - cron_df['start_time'])/np.timedelta64(1, 's')
 
-    proc_df = proc_df.sort_values(by='start_time', ascending=False)
-    cron_df = cron_df.sort_values(by='start_time', ascending=False)
+        proc_df = proc_df.sort_values(by='start_time', ascending=False)
+        cron_df = cron_df.sort_values(by='start_time', ascending=False)
 
-    # calculating values
-    proc_total_call = proc_df.shape[0]
-    proc_succ_call = proc_df[proc_df['status']=='success'].shape[0]
-    proc_fail_call = proc_df[proc_df['status']=='failed'].shape[0]
+        # calculating values
+        proc_total_call = proc_df.shape[0]
+        proc_succ_call = proc_df[proc_df['status']=='success'].shape[0]
+        proc_fail_call = proc_df[proc_df['status']=='failed'].shape[0]
 
-    cron_total_call = cron_df.shape[0]
-    cron_succ_call = cron_df[cron_df['status'] == 'success'].shape[0]
-    cron_fail_call = cron_df[cron_df['status'] == 'failed'].shape[0]
+        cron_total_call = cron_df.shape[0]
+        cron_succ_call = cron_df[cron_df['status'] == 'success'].shape[0]
+        cron_fail_call = cron_df[cron_df['status'] == 'failed'].shape[0]
 
-    response_json = {
-        'proc': {
-            'total_call': proc_total_call,
-            'succ_call': proc_succ_call,
-            'failed_call': proc_fail_call,
-            'data': proc_df.head(5).values.tolist()
-        },
-        'cron': {
-            'total_call': cron_total_call,
-            'succ_call': cron_succ_call,
-            'failed_call': cron_fail_call,
-            'data': cron_df.head(5).values.tolist()
+        response_json = {
+            'proc': {
+                'total_call': proc_total_call,
+                'succ_call': proc_succ_call,
+                'failed_call': proc_fail_call,
+                'data': proc_df.head(5).values.tolist()
+            },
+            'cron': {
+                'total_call': cron_total_call,
+                'succ_call': cron_succ_call,
+                'failed_call': cron_fail_call,
+                'data': cron_df.head(5).values.tolist()
+            }
         }
-    }
-    print("====response=====", response_json)
-    return jsonify(response_json)
+        print("====response=====", response_json)
+        return jsonify(response_json)
+    except Exception as e:
+        return jsonify({}), 401
 
 
 @app.route('/weather/', methods=['GET'])
 def weather():
-    latest_weather_data = db_conn.weather_data['city_max_temp'].find({}).sort([('created_time', -1)]).limit(1)
-    for weather_data in latest_weather_data:
-        weather_data.pop('_id',None)
-        temp_list = [temp['max_temp'] for temp in weather_data['city_temp']]
-        city_list = [temp['city'] for temp in weather_data['city_temp']]
-        weather_data['max'] = weather_data['city_temp'][temp_list.index(max(temp_list))]
-        weather_data['min'] = weather_data['city_temp'][temp_list.index(min(temp_list))]
-        weather_data['cities'] = city_list
-        weather_data['temps'] = temp_list
-        return jsonify(weather_data)
+    try:
+        latest_weather_data = db_conn.weather_data['city_max_temp'].find({}).sort([('created_time', -1)]).limit(1)
+        for weather_data in latest_weather_data:
+            weather_data.pop('_id',None)
+            temp_list = [temp['max_temp'] for temp in weather_data['city_temp']]
+            city_list = [temp['city'] for temp in weather_data['city_temp']]
+            weather_data['max'] = weather_data['city_temp'][temp_list.index(max(temp_list))]
+            weather_data['min'] = weather_data['city_temp'][temp_list.index(min(temp_list))]
+            weather_data['cities'] = city_list
+            weather_data['temps'] = temp_list
+            return jsonify(weather_data)
+    except Exception as e:
+        return jsonify({}), 401
 
 @app.errorhandler(404)
 def not_found(error):
