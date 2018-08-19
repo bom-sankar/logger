c29a782b6fa79e47d6059ea49af754142ca0047f
sanky-cse <sankar@prmfincon.com>
2018-08-19 23:04:50 +0530

    Pages redesigned using bootstrap

--- src/front-service/app/static/js/feed.js
+++ src/front-service/app/static/js/feed.js
@@ -1,42 +1,22 @@
-function dashboard(data_json) {
-    if ($("#dashboard-lineChart-2-type").length) {
-      var lineChartCanvas = $("#dashboard-lineChart-2-type").get(0).getContext("2d");
-      var lineChart = new Chart(lineChartCanvas, {
-        type: 'bar',
-        data: {
-          labels: data_json['cities'],
-          datasets: [{
-            data: data_json['temps'],
-            pointBackgroundColor: "#fffff",
-            pointBorderWidth: 1,
-            backgroundColor: [
-              '#3B579D',
-              '#1DA1F2',
-              '#FF5722'
-            ],
-            borderColor: [
-              '#ffffff'
-            ],
-            borderWidth: 1
-          }]
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
         },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false,
-            position:'left'
-          },
-          tooltips: {
-            enabled: true
-          },
-          layout: {
-            padding: {
-              top: 5,
-              bottom: 5
+        title: {
+            text: 'City Temperature'
+        },
+        xAxis: {
+            categories: {{ return_obj['cities'] }}
+        },
+        yAxis: {
+            title: {
+                text: 'Temp in K'
             }
-          }
-        }
-      });
-    }
-    }
+        },
+        series: [{
+            name: 'Temp',
+            data: {{ return_obj['temps'] }}
+        }],
+    });
+});
